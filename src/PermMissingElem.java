eb3bb1cdc1c05e2b879fec4daa4f6bd95452149f
배철한 <joker10421@gmail.com>
2018-11-02 16:21:49 +0900

    PermMissingElem logic change

--- src/PermMissingElem.java
+++ src/PermMissingElem.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PermMissingElem {
@@ -6,29 +5,24 @@ public class PermMissingElem {
 		int[] A = {2,3,1,5};
 		System.out.println(solution(A));
 	}
-	
+
 	public static int solution(int[] A) {
-        // write your code in Java SE 8
 		Arrays.sort(A);
 		int result = 1;
+
 		if(A.length == 0)
 			return 1;
-		
-		ArrayList<Integer> container = new ArrayList<>();
-		for(int val : A)
-			container.add(val);
-		
+
 		int i = 0;
-		
-		while(i < container.size()) {
-			if (result != container.get(i)) {
+
+		while(i < A.length) {
+			if (result != A[i]) 
 				break;
-			}
+
 			i++;
 			result++;
 		}
-		
-		
+
 		return result;
-    }
+	}
 }
