a7bec2be96aaf02e59e25113aa31252d2bf78940
배철한 <joker10421@gmail.com>
2020-08-27 00:22:34 +0900

    .

--- src/Combination.java
+++ src/Combination.java
@@ -1,27 +1,33 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Combination {
 	private static int count = 0;
+	private static List<List<Integer>> resultList = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		String input = "apdkf35k47n1t2";
 		solution(input);
-		System.out.println(count);
-		
+		System.out.println(String.format("output %d\n", count));
+	
+		String result = resultList
+				.stream()
+				.map(e -> e.stream().map(it -> String.valueOf(it)).collect(Collectors.joining(",","","")))
+				.collect(Collectors.joining(" / ", "(", ")"));
+		System.out.println(result);
 	}
 	
 	private static void solution(String input) {
 		String[] inputArr = input.split("");
-		// 문자열에서 정수 추출 
 		List<Integer> lst = Arrays.asList(inputArr)
 									.stream()
 									.filter(e -> e.matches("[0-9]"))
 									.map(Integer::parseInt)
 									.collect(Collectors.toList());
-		
+		Collections.sort(lst);
 		boolean[] visited = new boolean[lst.size()];
 		
 		for (int i = 1; i <= lst.size(); i++) {
@@ -30,16 +36,10 @@ public class Combination {
 		
 	}
 
-	/**
-	 * @param lst : 조합을 뽑아낼 List
-	 * @param visited
-	 * @param start : 기준
-	 * @param n : 배열의 길이
-	 * @param r : 조합의 길이
-	 */
+	
 	private static void combination(List<Integer> lst, boolean[] visited, int start, int n, int r) {
 	    if(r == 0) {
-	        print(lst, visited, n);
+	    	collect(lst, visited, n);
 	        return;
 	    } 
 
@@ -50,7 +50,7 @@ public class Combination {
 	    }
 	}
 	
-	private static void print(List<Integer> lst, boolean[] visited, int n) {
+	private static void collect(List<Integer> lst, boolean[] visited, int n) {
 		
 		List<Integer> result = new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -61,7 +61,7 @@ public class Combination {
 
         if(result.stream().mapToInt(e -> e).sum() == 10) {
         	count++;
-        	System.out.println(result);
+        	resultList.add(result);
         }
         
     }
