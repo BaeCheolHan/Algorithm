1765c2bdafb604de414ce4389fc24aec31c0200a
BaeCheolHan <joker10421@gmail.com>
2023-08-22 18:22:13 +0900

    solution permcheck

--- src/codility/lesson4/elements/PermCheck.java
+++ src/codility/lesson4/elements/PermCheck.java
@@ -11,8 +11,12 @@ public class PermCheck {
 	public static int solution(int[] A) {
 		Arrays.sort(A);
 
-		for(int i = 1; i < A.length; i++) {
-			if(A[i-1] != i) {
+		if(A[0] != 1) {
+			return 0;
+		}
+
+		for(int i = 0; i < A.length; i++) {
+			if(A[i] !=  i+1) {
 				return 0;
 			}
 		}
